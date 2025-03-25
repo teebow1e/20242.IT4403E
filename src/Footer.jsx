b0da7cfec24e0fcecbc16c0c708936cb958f0145
bananagobananza <hongquan090804@gmail.com>
2025-03-25 21:58:41 +0700

    Finishing Main page html

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,41 +3,43 @@ import './Footer.css'
 function Footer () {
     return (
         <footer class="footer">
-        <div className="footer-container">
-            <div className="social">
-                <a href="https://spotify.com">
-                    <img src="/social-spotify.svg" alt="" />
-                </a>
-                <a href="https://facebook.com">
-                    <img src="/social-facebook.svg" alt="" />
-                </a>
-                <a href="https://pinterest.com">
-                    <img src="/social-pinterest.svg" alt="" />
-                </a>
-                <a href="https://instagram.com">
-                    <img src="/social-instagram.svg" alt="" />
-                </a>
-                <a href="https://youtube.com">
-                    <img src="/social-youtube.svg" alt="" />
-                </a>
-                <a href="https://twitter.com">
-                    <img src="/social-twitter.svg" alt="" />
-                </a>
-            </div>
+            <div className='seperator'></div>
+            <div className="footer-container">
+                <div className="social">
+                    <a href="https://spotify.com">
+                        <img src="/social-spotify.svg" alt="" />
+                    </a>
+                    <a href="https://facebook.com">
+                        <img src="/social-facebook.svg" alt="" />
+                    </a>
+                    <a href="https://pinterest.com">
+                        <img src="/social-pinterest.svg" alt="" />
+                    </a>
+                    <a href="https://instagram.com">
+                        <img src="/social-instagram.svg" alt="" />
+                    </a>
+                    <a href="https://youtube.com">
+                        <img src="/social-youtube.svg" alt="" />
+                    </a>
+                    <a href="https://twitter.com">
+                        <img src="/social-twitter.svg" alt="" />
+                    </a>
+                </div>
 
-            <div className="more-info">
-                <p>Privacy Notice</p>
-                <p>Consumer Health Privacy Notice</p>
-                <p>Terms of Use</p>
-                <p>Do Not Share My Personal Information</p>
-                <p>CA Supply Chain Act</p>
-                <p>Accessibility</p>
-                <p>Cookie Preferences</p>
-            </div>
-            <p className="copyright">© {new Date().getFullYear()} Starbucks Coffee Company. All rights reserved.</p>
+                <div className="more-info">
+                    <a href="/#">Privacy Notice</a>
+                    <a href="/#">Consumer Health Privacy Notice</a>
+                    <a href="/#">Terms of Use</a>
+                    <a href="/#">Do Not Share My Personal Information</a>
+                    <a href="/#">CA Supply Chain Act</a>
+                    <a href="/#">Accessibility</a>
+                    <a href="/#">Cookie Preferences</a>
+                </div>
 
-        </div>
-    </footer>
+                <p className="copyright">© {new Date().getFullYear()} Starbucks Coffee Company. All rights reserved.</p>
+
+            </div>
+        </footer>
     )
 }
 
--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -4,7 +4,7 @@ function HomeScreen () {
     return (
         <>
             {/* <!-- Box A --> */}
-            <section className="box box-a bg-primary text-center py-md">
+            <section className="box box-a bg-box-a text-center py-md">
                 <div className="box-inner">
                     <h2 className="text-xl">Jingle all the way to free favorites</h2>
                     <p className="text-md">
@@ -16,40 +16,54 @@ function HomeScreen () {
             </section>
 
             {/* <!-- Box B --> */}
-            <section className="box box-b bg-secondary grid-col-2">
-                <img src="/HomeScreen1.jpg" alt="" />
-                <div className="box-text">
-                    <h2 className="text-xl">New to the nice list</h2>
-                    <p className="text-md">
-                    For a nondairy twist on a holiday cookie classic, try the new Iced Sugar Cookie Almondmilk Latte.
-                    </p>
-                    <a href="#" className="btn btn-light-outline">Order Now</a>
-                </div>
-            </section>
-
-            {/* <!-- Box C --> */}
-            <section className="box box-c bg-secondary grid-col-2 grid-reversed">
+            <section className="box box-b bg-box-b grid-col-2">
                 <img src="/HomeScreen1.jpg" alt="" />
                 <div className="box-text">
                     <h2 className="text-xl">The Spring Edit</h2>
                     <p className="text-md">
                     Fresh flavors, familiar joy.
                     </p>
-                    <a href="#" className="btn btn-light-outline">Order Now</a>
+                    <a href="#" className="btn btn-light-outline">View the menu</a>
+                </div>
+            </section>
+
+            {/* <!-- Box C --> */}
+            <section className="box box-c bg-box-c grid-col-2 grid-reversed">
+                <img src="/HomeScreen3.jpg" alt="" />
+                <div className="box-text">
+                    <h2 className="text-xl">Our smoothest, brightest coffee yet</h2>
+                    <p className="text-md">
+                    Discover the people and places behind the new Starbucks® Sunsera Blend™ as it makes its way from farm to cup.
+                    </p>
+                    <a href="#" className="btn btn-light-outline">See the journey</a>
                 </div>
             </section>
 
             {/* <!-- Box D --> */}
-            <section className="box box-d bg-primary grid-col-2">
-                <img src="/HomeScreen3.jpg" alt="" />
+            <section className="box box-d bg-box-d grid-col-2">
+                <img src="/HomeScreen2.jpg" alt="" />
                 <div className="box-text">
-                    <h2 className="text-xl">It’s a great day for free coffee</h2>
-                    <p className="text-md">
+                    <h2 className="text-lg">It’s a great day for free coffee</h2>
+                    <p className="text-sm">
                     Start your Starbucks® Rewards journey with a coffee on us. Join now and enjoy a free handcrafted drink with a qualifying purchase during your first week.
                     </p>
                     <a href="#" className="btn btn-light-outline">Order Now</a>
                 </div>
             </section>
+
+            {/* <!-- Section E --> */}
+            <section className="box box-e bg-box-e">
+                <div className="box-text">
+                    <p className="text-esm">
+                    *Valid for new Starbucks Rewards members for 7 days from sign up. Coupon will be available in the offers tab of your Starbucks app following sign up and may take up to 48 hours to arrive. Good at participating U.S. stores for a handcrafted menu-sized beverage with qualifying purchase ($8 max value). Qualifying purchase excludes alcohol, Starbucks Card and Card reloads. Limit one. Cannot be combined with other offers or discounts. Excludes delivery services. Sign up before 3/30/2025.
+                    </p>
+                    <br></br>
+                    <p className="text-esm">
+                    **Free refills of hot and iced brewed coffee or tea during same store visit. Excludes Cold Brew, Nitro Cold Brew, Iced Tea Lemonade, and flavored Iced Tea and Starbucks Refreshers® base. At participating stores. Want a free starbuck ? Ask FieryPhoenix !
+                    </p>
+                </div>
+            </section>
+
         </>
     )
 }
