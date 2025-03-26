79016cacbf84c57a8f9f8cb6b1b93f743b010080
bananagobananza <hongquan090804@gmail.com>
2025-03-26 22:02:20 +0700

    Adding navigating code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Header';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './Layout';
 import HomeScreen from './HomeScreen';
-import Footer from './Footer';
+import LoginScreen from './LoginScreen';
+import SignupScreen from './SignupScreen';
+import MenuScreen from './MenuScreen';
 import './App.css';
+
+const user = null; // or use state/context to manage login
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route 
-          path="/" 
+        {/* Main layout with home screen */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomeScreen />} />
+        </Route>
+
+        {/* Sign in route with redirect if logged in */}
+        <Route
+          path="/account/signin"
           element={
-            <>
-              <Header></Header>
-              <HomeScreen></HomeScreen>
-			        <Footer></Footer>
-            </>
+            user ? <Navigate to="/menu" replace /> : <LoginScreen />
+          }
+        />
+
+        {/* Sign up route with redirect if logged in */}
+        <Route
+          path="/account/create"
+          element={
+            user ? <Navigate to="/menu" replace /> : <SignupScreen />
+          }
+        />
+
+        {/* Menu page only for logged-in users */}
+        <Route
+          path="/menu"
+          element={
+            user ? (
+              <Layout>
+                <MenuScreen />
+              </Layout>
+            ) : (
+              <Navigate to="/account/signin" replace />
+            )
           }
         />
       </Routes>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,93 +1,74 @@
 import { useEffect } from 'react';
-import './Header.css'
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
-    useEffect(() => {
-        const btn = document.getElementById('menu-btn');
-        const nav = document.getElementById('menu');
-    
-        function navToggle() {
-            btn.classList.toggle('open');
-            nav.classList.toggle('hidden');
-            document.body.classList.toggle('no-scroll');
-        }
-    
-        btn.addEventListener('click', navToggle);
-    
-        return () => {
-            btn.removeEventListener('click', navToggle);
-        };
-      }, []);
+  useEffect(() => {
+    const btn = document.getElementById('menu-btn');
+    const nav = document.getElementById('menu');
 
-    return (
-        <>
-            <header>
-                <nav className="navbar">
-                    <div className="navbar-container">
-                        <div className="navbar-brand">
-                            <a href="index.html">
-                                <img src="public/logo.svg" alt="Starbucks" />
-                            </a>
-                        </div>
+    function navToggle() {
+      btn.classList.toggle('open');
+      nav.classList.toggle('hidden');
+      document.body.classList.toggle('no-scroll');
+    }
 
-                        <ul className="navbar-nav-left">
-                            <li>
-                                <a href="#">Menu</a>
-                            </li>
-                            <li>
-                                <a href="#">Rewards</a>
-                            </li>
-                            <li>
-                                <a href="#">Gift Cards</a>
-                            </li>
-                        </ul>
+    btn.addEventListener('click', navToggle);
 
-                        <ul className="navbar-nav-right">
-                            {/* <li>
-                                <a href="#">
-                                    <img src="public/marker.svg" alt="" />
-                                    <span>Find a store</span>
-                                </a>
-                            </li> */}
-                            <li>
-                                <button className="btn btn-dark-outline">Sign in</button>
-                            </li>
-                            <li>
-                                <button className="btn btn-dark">Join now</button>
-                            </li>
-                        </ul>
+    return () => {
+      btn.removeEventListener('click', navToggle);
+    };
+  }, []);
 
-                        {/* Hamburger Menu */}
-                        <button type="button" className="hamburger" id="menu-btn">
-                            <span className="hamburger-top"></span>
-                            <span className="hamburger-middle"></span>
-                            <span className="hamburger-bottom"></span>
-                        </button>
-                    </div>
-                </nav>
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-brand">
+            <Link to="/">
+              <img src="/logo.svg" alt="Starbucks" />
+            </Link>
+          </div>
 
-                {/* Mobile Menu */}
-                <div class="mobile-menu hidden" id="menu">
-                    <ul>
-                        <li> <a href="#">Menu</a> </li>
-                        <li> <a href="#">Rewards</a> </li>
-                        <li> <a href="#">Gift Cards</a> </li>
-                    </ul>
+          <ul className="navbar-nav-left">
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="#">Rewards</Link></li>
+            <li><Link to="#">Gift Cards</Link></li>
+          </ul>
 
-                    <div className="mobile-menu-bottom">
-                        <button className="btn btn-dark-outline">Sign in</button>
-                        <button className="btn btn-dark">Join now</button>
-                        {/* <div>
-                            <a href="#">
-                                <img src="img/marker.svg" alt="" />
-                                <span>Find a store</span>
-                            </a>
-                        </div> */}
-                    </div>
-                </div>
-            </header>
-        </>
-    );
+          <ul className="navbar-nav-right">
+            <li>
+              <Link to="/account/signin" className="btn ">Sign in</Link>
+            </li>
+            <li>
+              <Link to="/account/create" className="btn btn-dark">Join now</Link>
+            </li>
+          </ul>
+
+          {/* Hamburger Menu */}
+          <button type="button" className="hamburger" id="menu-btn">
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
+        </div>
+      </nav>
+
+      {/* Mobile Menu */}
+      <div className="mobile-menu hidden" id="menu">
+        <ul>
+          <li><Link to="#">Menu</Link></li>
+          <li><Link to="#">Rewards</Link></li>
+          <li><Link to="#">Gift Cards</Link></li>
+        </ul>
+
+        <div className="mobile-menu-bottom">
+          <Link to="/account/signin" className="btn btn-dark-outline">Sign in</Link>
+          <Link to="/account/create" className="btn btn-dark">Join now</Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
