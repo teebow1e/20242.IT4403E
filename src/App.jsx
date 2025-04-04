488c69b1426077509e9461c840c9503f9b775d00
meadowilla <nguyen8a1yp@gmail.com>
2025-04-04 21:59:10 +0700

    feat: authentication when login

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,35 @@ import HomeScreen from './HomeScreen';
 import LoginScreen from './LoginScreen';
 import SignupScreen from './SignupScreen';
 import MenuScreen from './MenuScreen';
+import {selectUser} from './features/UserSlice';
+import {auth} from './firebase';
+import {login, logout} from './features/UserSlice'; 
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
-const user = null; // or use state/context to manage login
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // User is signed in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName
+        }));
+      } else {
+        // User is signed out
+        dispatch(logout());
+      }
+    }
+    );
+  }, [dispatch]);
+
   return (
     <Router>
       <Routes>
@@ -27,7 +51,7 @@ function App() {
 
         {/* Sign up route with redirect if logged in */}
         <Route
-          path="/account/create"
+          path="/account/signup"
           element={
             user ? <Navigate to="/menu" replace /> : <SignupScreen />
           }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import store from './store'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
+// import * as serviceWorker from './serviceWorker.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
