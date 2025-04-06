52bd30f4fbdb474b38853de0b3c9fb47750de40c
meadowilla <nguyen8a1yp@gmail.com>
2025-04-07 00:06:25 +0700

    feat: add Logout

---
+++ src/useIdleLogout.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase";
+import { useNavigate } from "react-router-dom";
+
+const IDLE_TIMEOUT = 15 * 60 * 1000; // 15 mins 
+
+function useIdleLogout(){
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        let timer;
+
+        const resetTimer = () => {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                signOut(auth).then(() => {
+                alert("You were inactive. Logged out for security.");
+                navigate("/account/login");
+                });
+            }, IDLE_TIMEOUT);
+        };
+
+        // Events that count as "activity"
+        const events = ["mousemove", "keydown", "click", "scroll"];
+        events.forEach(event => window.addEventListener(event, resetTimer));
+
+        resetTimer(); // start timer on mount
+        return () => {
+            clearTimeout(timer);
+            events.forEach(event => window.removeEventListener(event, resetTimer));
+        };
+    }, []);
+}
+
+export default useIdleLogout;
