2eeb22eb2df2a155c484011652ad034c4698d6fc
FieryPhoenix <70960874+bananagobananza@users.noreply.github.com>
2025-04-10 21:18:03 +0700

    finish elements of menu/featured

--- src/screens/FeaturedScreen.jsx
+++ src/screens/FeaturedScreen.jsx
@@ -3,78 +3,75 @@ import "./FeaturedScreen.css"
 function Featured() {
     return (
         <>
-            <h1>Featured</h1>
+            <h1 className="title"> The Spring edit</h1>
             {/* <!-- Box A --> */}
-            <section className="box box-a bg-box-a text-center py-md">
-                <div className="box-inner">
-                    <h2 className="text-xl">Jingle all the way to free favorites</h2>
-                    <p className="text-md">
-                    Join Starbucks ® Rewards for delicious deals & exclusive offers.
-                    <br></br>
-                    <a href="#">Learn more</a>
-                    </p>
+            <section className="box box-a bg-box-a grid-col-2">
+                <div className="box-inner box-a-inner">
+                    <img src="/drink1.jpg" alt="" />
+                    <h2 className="text-md text-drink-header-a">Iced Lavender Cream Oatmilk Matcha</h2>
+                    <p className="text-sm text-drink-description-a" >A smooth combination of matcha green tea and oatmilk, finished with lavender cream cold foam with subtle floral notes. Includes dairy.</p>
+                    <a href="#" className="btn btn-green-outline btn-order">Order now</a>
                 </div>
+
+                <div className="box-inner box-a-inner">
+                    <img src="/drink2.jpg" alt="" />
+                    <h2 className="text-md text-drink-header-a">Iced Lavender Oatmilk Latte</h2>
+                    <p className="text-sm text-drink-description-a">Starbucks® Blonde Espresso and oatmilk combine with subtle floral accents, served over ice. Exclusive only in rush hour, weekdays & special occasions.</p>
+                    <a href="#" className="btn btn-green-outline btn-order">Order now</a>
+                </div>
+
             </section>
 
             {/* <!-- Box B --> */}
             <section className="box box-b bg-box-b grid-col-2">
-                <img src="/HomeScreen1.jpg" alt="" />
+                <img src="/drink3.jpg" alt="" />
                 <div className="box-text">
-                    <h2 className="text-xl">The Spring Edit</h2>
-                    <p className="text-md">
-                    Fresh flavors, familiar joy.
+                    <h2 className="text-md">New Iced Cherry Chai</h2>
+                    <p className="text-sm">
+                    A creamy cold foam with notes of cherry and a crunchy topping meets our signature chai tea latte for a spring take on a favorite.
                     </p>
-                    <a href="#" className="btn btn-light-outline">View the menu</a>
+                    <a href="#" className="btn btn-light-outline btn-order">Order now</a>
                 </div>
             </section>
 
             {/* <!-- Box C --> */}
-            <section className="box box-c bg-box-c grid-col-2 grid-reversed">
-                <img src="/HomeScreen3.jpg" alt="" />
-                <div className="box-text">
-                    <h2 className="text-xl">Our smoothest, brightest coffee yet</h2>
-                    <p className="text-md">
-                    Discover the people and places behind the new Starbucks® Sunsera Blend™ as it makes its way from farm to cup.
-                    </p>
-                    <a href="#" className="btn btn-light-outline">See the journey</a>
+            <section className="box box-c bg-box-c grid-col-2">
+                <div className="box-inner box-c-inner">
+                    <img src="/drink4.jpg" alt="" />
+                    <h2 className="text-md text-drink-header-c">Cortado</h2>
+                    <p className="text-sm text-drink-description-c" >Three ristretto shots of Starbucks® Blonde Espresso combined with steamed whole milk and served in an 8 fl oz short cup.</p>
+                    <a href="#" className="btn btn-light-outline btn-order">Order now</a>
+                </div>
+
+                <div className="box-inner box-c-inner">
+                    <img src="/drink5.jpg" alt="" />
+                    <h2 className="text-md text-drink-header-c">Brown Sugar Oatmilk Cortado</h2>
+                    <p className="text-sm text-drink-description-c">Three ristretto shots of Starbucks® Blonde Espresso, brown sugar syrup, cinnamon and steamed oatmilk in an 8 fl oz short cup.</p>
+                    <a href="#" className="btn btn-light-outline btn-order">Order now</a>
                 </div>
             </section>
 
             {/* <!-- Box D --> */}
-            <section className="box box-d bg-box-d grid-col-2">
-                <img src="/HomeScreen2.jpg" alt="" />
+            <section className="box box-d bg-box-d grid-col-2 grid-reversed">
+                <img src="/drink6.jpg" alt="" />
                 <div className="box-text">
-                    <h2 className="text-lg">It’s a great day for free coffee</h2>
+                    <h2 className="text-md">Blackberry Sage Refreshers</h2>
                     <p className="text-sm">
-                    Start your Starbucks® Rewards journey with a coffee on us. Join now and enjoy a free handcrafted drink with a qualifying purchase during your first week.*
+                    Sweet blackberry flavors and notes of sage are shaken with real blackberry pieces—enjoyed alone or with lemonade or coconutmilk.
                     </p>
-                    <a href="#" className="btn btn-light-outline">Order Now</a>
+                    <a href="#" className="btn btn-light-outline">See our seasonal refreshers</a>
                 </div>
             </section>
 
             {/* <!-- Box E --> */}
-            <section className="box box-e bg-box-e grid-col-2 grid-reversed">
-                <img src="/HomeScreen5.jpg" alt="" />
+            <section className="box box-e bg-box-e grid-col-2">
+                <img src="/drink7.jpg" alt="" />
                 <div className="box-text">
-                    <h2 className="text-xl">More reasons to stay awhile</h2>
+                    <h2 className="text-md">New Spicy Falafel Pocket and New Jalapeño Chicken Pocket</h2>
                     <p className="text-sm">
-                    Mugs, glasses and the condiment bar are back—and get free refills of hot or iced brewed coffee or tea.**
+                    Hand-folded, toasted lavash flatbreads with well-balanced, savory fillings like falafel and spicy herb sauce or chicken, peppers and jalapeño cream cheese.
                     </p>
-                    <a href="#" className="btn btn-light-outline">Learn More</a>
-                </div>
-            </section>
-
-
-            {/* <!-- Section E --> */}
-            <section className="box">
-                <div className="box-text">
-                    <p className="text-esm">
-                    *Valid for new Starbucks Rewards members for 7 days from sign up. Coupon will be available in the offers tab of your Starbucks app following sign up and may take up to 48 hours to arrive. Good at participating U.S. stores for a handcrafted menu-sized beverage with qualifying purchase ($8 max value). Qualifying purchase excludes alcohol, Starbucks Card and Card reloads. Limit one. Cannot be combined with other offers or discounts. Excludes delivery services. Sign up before 3/30/2025.
-                    </p>
-                    <br></br>
-                    <p className="text-esm">
-                    **Free refills of hot and iced brewed coffee or tea during same store visit. Excludes Cold Brew, Nitro Cold Brew, Iced Tea Lemonade, and flavored Iced Tea and Starbucks Refreshers® base. At participating stores. Want a free starbuck ? Ask FieryPhoenix !
-                    </p>
+                    <a href="#" className="btn btn-light-outline">Order now</a>
                 </div>
             </section>
 
