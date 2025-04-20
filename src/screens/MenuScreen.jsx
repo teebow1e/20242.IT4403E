594d49528dc1ea143c222b771b278ec706040fe2
FieryPhoenix <70960874+bananagobananza@users.noreply.github.com>
2025-04-20 21:50:07 +0700

    Add jsxs related to menu

--- src/screens/MenuScreen.jsx
+++ src/screens/MenuScreen.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import useIdleLogout from "../useIdleLogout";
 
+import MenuHeader from "./menu/MenuHeader";
+import MenuList from "./menu/MenuList";
+// import MenuItem from "./menu/MenuItem";
+// import menuList from "./menu/menuList.json";
+
 function MenuScreen() {
-  useIdleLogout();
   return (
-    <div>
-      <h1>Menu Screen</h1>
-      <p>This is the main menu for logged-in users.</p>
+    <div className="flex flex-col mb-[100px] max-md:mt-0 max-[425px]:mt-[30px]">
+      <MenuHeader />
+      <div className="flex gap-[100px] pl-[131px] mt-[101px] max-md:mt-[30px] max-[425px]:pl-[20px]">
+        <div className="max-md:hidden">
+          <MenuList />
+        </div>
+        
+      </div>
     </div>
   );
 }
+
 
 export default MenuScreen;
