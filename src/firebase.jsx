249e3319d50c8bc112aafdeebced3fed264dc5a4
Trung Tran <tritrungtq@gmail.com>
2025-04-29 02:04:39 +0000

    formatting

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,5 +1,5 @@
-import {initializeApp} from "firebase/app";
-import { getAuth} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA0DUtxPP0_EIHRd_vEKKxpRpXhjmp7H6k",
