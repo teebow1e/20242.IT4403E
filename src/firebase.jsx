5700fa072b29f185d928bc0ed3a3a423c7f6a844
Trung Tran <tritrungtq@gmail.com>
2025-05-02 15:44:52 +0000

    finish passing submit data to firebase

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,11 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA0DUtxPP0_EIHRd_vEKKxpRpXhjmp7H6k",
     authDomain: "it4403e.firebaseapp.com",
     projectId: "it4403e",
     storageBucket: "it4403e.firebasestorage.app",
+    databaseURL: "https://it4403e-default-rtdb.asia-southeast1.firebasedatabase.app/",
     messagingSenderId: "16977471090",
     appId: "1:16977471090:web:a6aaf08fccedf46cf644eb",
     measurementId: "G-730QK8HM9C"
@@ -14,3 +16,6 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 export const auth = getAuth(firebaseApp);
+export const database = getDatabase(firebaseApp);
+
+export default firebaseApp;
