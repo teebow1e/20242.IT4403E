8128cad65d7c4a018fecdd3656d6feb95153820c
meadowilla <nguyen8a1yp@gmail.com>
2025-05-16 20:59:10 +0700

    for checkout ;<

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,18 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDatabase } from 'firebase/database';
+import {initializeApp} from "firebase/app";
+import { getAuth} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA0DUtxPP0_EIHRd_vEKKxpRpXhjmp7H6k",
     authDomain: "it4403e.firebaseapp.com",
     projectId: "it4403e",
-    databaseURL: 'https://it4403e-default-rtdb.asia-southeast1.firebasedatabase.app/',
     storageBucket: "it4403e.firebasestorage.app",
     messagingSenderId: "16977471090",
     appId: "1:16977471090:web:a6aaf08fccedf46cf644eb",
     measurementId: "G-730QK8HM9C"
 };
 
-export const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
 export const auth = getAuth(firebaseApp);
-export const db = getDatabase(firebaseApp);
