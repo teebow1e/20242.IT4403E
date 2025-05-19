9543967e9bab19367b5673df4975c736fe0653be
meadowilla <nguyen8a1yp@gmail.com>
2025-05-19 10:06:31 +0700

    rbac-v2

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ function App() {
                 <Route 
                     path="/menu" 
                     element={
-                      <ProtectedRoute>
+                      <ProtectedRoute allowedRole={["customer"]}>
                           <Layout menuPage={true}/>
                       </ProtectedRoute> 
                     }
