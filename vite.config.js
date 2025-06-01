dcf9833e9008b7e3bdc87e597675da43e9dc0b3a
Trung Tran <tritrungtq@gmail.com>
2025-06-01 03:51:22 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     },
 
     workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      globPatterns: ['**/*.{js,css,html,svg,png,ico,ttf,woff,woff2}'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
     },
